8acc878d7df8f4c8e87480a58ab9ef5c9e8f69f3
Prakosa <prakosa.ananta@gmail.com>
2022-07-08 16:38:06 +0700

    fix: error when build xcode

--- ios/Networking/PaymentsErrorResponse.swift
+++ ios/Networking/PaymentsErrorResponse.swift
@@ -3,27 +3,27 @@ import AdyenNetworking
 import Foundation
 
 internal struct PaymentsErrorResponse: Codable, Error, LocalizedError {
-        
+
     public let status: Int?
-    
+
     public let errorCode: String?
-    
+
     public let message: String?
-    
+
     public let type: APIErrorType?
-    
+
     public var errorDescription: String? {
         message
     }
-    
+
     internal let resultCode: ResultCode?
-    
+
     internal let refusalReason: String?
-    
+
     internal let refusalReasonCode: String?
-    
+
     internal let additionalData: AdditionalData?
-    
+
     private enum CodingKeys: String, CodingKey {
         case resultCode
         case status
@@ -34,19 +34,19 @@ internal struct PaymentsErrorResponse: Codable, Error, LocalizedError {
         case refusalReasonCode
         case additionalData
     }
-    
+
     internal init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
-        self.resultCode = try? container.decodeIfPresent(ResultCode.self, forKey: .resultCode)
-        self.status = try? container.decodeIfPresent(Int.self, forKey: .status)
-        self.errorCode = try? container.decodeIfPresent(String.self, forKey: .errorCode)
+        self.resultCode = try container.decodeIfPresent(ResultCode.self, forKey: .resultCode)
+        self.status = try container.decodeIfPresent(Int.self, forKey: .status)
+        self.errorCode = try container.decodeIfPresent(String.self, forKey: .errorCode)
         self.message = try container.decodeIfPresent(String.self, forKey: .message)
-        self.type = try? container.decodeIfPresent(APIErrorType.self, forKey: .type)
-        self.refusalReason = try? container.decodeIfPresent(String.self, forKey: .refusalReason)
-        self.refusalReasonCode = try? container.decodeIfPresent(String.self, forKey: .refusalReasonCode)
-        self.additionalData = try? container.decodeIfPresent(AdditionalData.self, forKey: .additionalData)
+        self.type = try container.decodeIfPresent(APIErrorType.self, forKey: .type)
+        self.refusalReason = try container.decodeIfPresent(String.self, forKey: .refusalReason)
+        self.refusalReasonCode = try container.decodeIfPresent(String.self, forKey: .refusalReasonCode)
+        self.additionalData = try container.decodeIfPresent(AdditionalData.self, forKey: .additionalData)
     }
-    
+
     internal func encode(to encoder: Encoder) throws {
         var container = encoder.container(keyedBy: CodingKeys.self)
         try container.encodeIfPresent(resultCode?.rawValue, forKey: .resultCode)
